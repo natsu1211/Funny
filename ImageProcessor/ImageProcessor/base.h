b9aa6f20383e0b70ecc183a580be00673bf84f5d
natsu1211 <longyutao1211@gmail.com>
2015-05-30 02:37:47 +0900

    Add Noncopyable interface

--- ImageProcessor/ImageProcessor/base.h
+++ ImageProcessor/ImageProcessor/base.h
@@ -5,7 +5,7 @@
 #define	_IMAGE_BASE_
 
 #include <stdint.h>
-
+#include <cstdio>
 
 typedef unsigned long DWORD;
 typedef unsigned short WORD;
@@ -14,6 +14,11 @@ typedef unsigned char BYTE;
 #ifndef _MSC_VER
 #define abstract
 #endif
+
+#if __cplusplus>=199711L 
+#define DELETA_FUNCTION_SUPPORTED
+#endif
+
 
 namespace Image
 {
@@ -25,6 +30,29 @@ namespace Image
 		virtual ~ImageProcessorBase();
 		virtual void LoadImages(const char*) = 0;
 		//virtual void SavaImages(const char*) = 0;
+
+	};
+
+	class Noncopyable
+	{
+	public:
+		Noncopyable(){};
+	#ifdef DELETA_FUNCTION_SUPPORTED
+		Noncopyable(const Noncopyable&) = delete ;
+		Noncopyable& operator=(const Noncopyable&) = delete;
+	#else
+	private:
+		Noncopyable(const Noncopyable&){};
+		Noncopyable& operator=(const Noncopyable&){return *this};
+	#endif
+	};
+
+
+	class FileBase : public Noncopyable
+	{
+	protected:
+		FILE *file;
+	//public:
 
 	};
 }
